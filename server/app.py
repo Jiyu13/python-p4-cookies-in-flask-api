5312a83c4401cf1e4ec4d321ffe5af46f9aaedc1
Ziru Zou <jiyucoding@yahoo.com>
2023-03-24 19:42:33 -0400

    add notes

--- server/app.py
+++ server/app.py
@@ -3,11 +3,19 @@ from flask import Flask, request, session, jsonify, make_response
 app = Flask(__name__)
 app.json.compact = False
 
+# Set the secret key to some random bytes. Keep this really secret!
+# used to sign session cookies for protection against cookie data tampering.
+# user can't modify the contents unless they know this secret key used for signing
+# in order to use session, need to set a secret key
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
+
+# or use os module to generate a good secret key from command line
+# $ python -c 'import os; print(os.urandom(16))'
 
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
 
+    # Flask takes the values in session obj and seializes them into a cookie
     # use ternary operator to set values for session
     # can be session.get("hello")/"World"
     session["hello"] = session.get("hello") or "World"
@@ -25,6 +33,9 @@ def show_session(key):
         'cookies': [{cookie: request.cookies[cookie]}
             for cookie in request.cookies],
     }), 200)
+    
+    # for cookie in request.cookies:
+    #     print(request.cookies[cookie]) 
 
     response.set_cookie('mouse', 'Cookie')
 
